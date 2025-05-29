0edcdd498e8e2a9c12741f7fe787d49784c4e04d
Zeb DeOs <zebdeos@bayprogrammer.com>
2025-05-29 10:29:40 -0400

    index.html: move to root of project

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ import mojo from '@mojojs/core';
 const app = mojo();
 
 app.get('/', async ctx => {
-  await ctx.sendFile(ctx.home.child('public', 'index.html'));
+  await ctx.sendFile(ctx.home.child('index.html'));
 })
+
+app.start();
