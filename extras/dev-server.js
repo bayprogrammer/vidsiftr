71de63a88567ef9bfe0b624b9fdf942424af0e77
Zeb DeOs <zebdeos@bayprogrammer.com>
2025-05-29 13:45:04 -0400

    dev-server: serve up modules from vendor dir

--- extras/dev-server.js
+++ extras/dev-server.js
@@ -3,21 +3,26 @@ import mojo from '@mojojs/core';
 const app = mojo()
 
 // serve up our spa html
-async function indexHtml(ctx) {
-  return await ctx.sendFile(ctx.home.child('../index.html'))
-}
-
 app.get('/', indexHtml)
 app.get('/index.html', indexHtml)
 
+async function indexHtml(ctx) {
+  await ctx.sendFile(ctx.home.child('..', 'index.html'))
+}
+
 // serve up our spa entrypoint
 app.get('/index.js', async ctx => {
-  await ctx.sendFile(ctx.home.child('../index.js'))
+  await ctx.sendFile(ctx.home.child('..', 'index.js'))
 })
 
 // serve up js modules
 app.get('/src/#file', async ctx => {
-  await ctx.sendFile(ctx.home.child('../src', ctx.stash.file))
+  await ctx.sendFile(ctx.home.child('..', 'src', ctx.stash.file))
+})
+
+// serve up vendored js modules
+app.get('/vendor/#file', async ctx => {
+  await ctx.sendFile(ctx.home.child('..', 'vendor', ctx.stash.file))
 })
 
 app.start()
