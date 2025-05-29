02a54a02b8176a9b17f822bc3359f26454b9f57f
Zeb DeOs <zebdeos@bayprogrammer.com>
2025-05-29 11:57:14 -0400

    dev-server.js: move to extras and rename from app.js...
    ...to clarify that this is just a tool for development rather than
    part of the production SPA itself.

--- extras/dev-server.js
+++ extras/dev-server.js
@@ -4,7 +4,7 @@ const app = mojo()
 
 // serve up our spa html
 async function indexHtml(ctx) {
-  return await ctx.sendFile(ctx.home.child('index.html'))
+  return await ctx.sendFile(ctx.home.child('../index.html'))
 }
 
 app.get('/', indexHtml)
@@ -12,12 +12,12 @@ app.get('/index.html', indexHtml)
 
 // serve up our spa entrypoint
 app.get('/index.js', async ctx => {
-  await ctx.sendFile(ctx.home.child('index.js'))
+  await ctx.sendFile(ctx.home.child('../index.js'))
 })
 
 // serve up js modules
 app.get('/src/#file', async ctx => {
-  await ctx.sendFile(ctx.home.child('src', ctx.stash.file))
+  await ctx.sendFile(ctx.home.child('../src', ctx.stash.file))
 })
 
 app.start()
