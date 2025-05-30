d910aa2b2be8e042df900445d7777f8e47194403
Zeb DeOs <zebdeos@bayprogrammer.com>
2025-05-30 10:29:00 -0400

    dev-server: tell browsers "no cache for you!"

--- extras/dev-server.js
+++ extras/dev-server.js
@@ -2,28 +2,37 @@ import mojo from '@mojojs/core';
 
 const app = mojo()
 
+function disableCache(ctx) {
+  ctx.res.set('Cache-Control', 'no-cache')
+}
+
 // serve up js modules
 app.get('/src/#file', async ctx => {
+  disableCache(ctx)
   await ctx.sendFile(ctx.home.child('..', 'src', ctx.stash.file))
 })
 
 // serve up vendored js modules
 app.get('/vendor/#file', async ctx => {
+  disableCache(ctx)
   await ctx.sendFile(ctx.home.child('..', 'vendor', ctx.stash.file))
 })
 
 // serve up our spa entrypoint
 app.get('/index.js', async ctx => {
+  disableCache(ctx)
   await ctx.sendFile(ctx.home.child('..', 'index.js'))
 })
 
 // redirect /index.html -> /
 app.get('/index.html', async ctx => {
+  disableCache(ctx)
   await ctx.redirectTo('/');
 })
 
 // serve up our spa html
 app.get('/*', async ctx => {
+  disableCache(ctx)
   await ctx.sendFile(ctx.home.child('..', 'index.html'))
 })
 
