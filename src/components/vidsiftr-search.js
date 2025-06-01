aac544c49af1c5b958c246b112057e7a87e1b070
Zeb DeOs <zebdeos@bayprogrammer.com>
2025-06-01 18:33:31 -0400

    VidsiftrSearch: manually test VidSiftrSearchResults...
    ...a bit more before moving on.

--- src/components/vidsiftr-search.js
+++ src/components/vidsiftr-search.js
@@ -9,13 +9,16 @@ export default class VidsiftrSearch extends LitElement {
 
     // TODO(zmd): temporary testing, remove once finished
     this.testResult = config.YouTubeDataApi.fixtures['javascript']
+    //this.testResult = {items: []}
+    //this.testResult = undefined
+    //this.testResult = null
   }
 
   render() {
     return html`
       <vidsiftr-search-box></vidsiftr-search-box>
       <vidsiftr-search-results
-        .items=${this.testResult.items}
+        .items=${this.testResult?.items}
       ></vidsiftr-search-results>
     `
   }
