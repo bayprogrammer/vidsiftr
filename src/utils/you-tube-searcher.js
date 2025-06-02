6c5504401d1e80a9d706de719c997f5ffe16a726
Zeb DeOs <zebdeos@bayprogrammer.com>
2025-06-02 01:04:46 -0400

    YouTubeSearcher: privatize certain methods

--- src/utils/you-tube-searcher.js
+++ src/utils/you-tube-searcher.js
@@ -1,20 +1,16 @@
 export default class YouTubeSearcher {
+  #lastResponseBody = null
+
   constructor(keywords, maxResults, apiKey, endpoint) {
     this.keywords = keywords
     this.maxResults = maxResults
     this.apiKey = apiKey
     this.endpoint = endpoint
-
-    // TODO(zmd): declare in class directly?
-    // TODO(zmd): privatize member
-    this._lastResponseBody = null
   }
 
   async fetchItems() {
     try {
-      // TODO(zmd): we need separate urls for seprate calls (search vs. single
-      //   video)
-      const response = await fetch(this.url)
+      const response = await fetch(this.#fetchItemsUrl)
       const status = response.status
       const body = await response.json()
 
@@ -23,8 +19,8 @@ export default class YouTubeSearcher {
         return []
       }
 
-      // TODO(zmd): privatize member
-      this._lastResponseBody = body
+      console.log('old last response body was: ' + this.#lastResponseBody)
+      this.#lastResponseBody = body
 
       return body.items
     } catch (e) {
@@ -37,15 +33,11 @@ export default class YouTubeSearcher {
   //  // TODO(zmd): implement me
   //}
 
-  // TODO(zmd): we need separate urls for separate calls (search vs. single
-  //   video)
-  // TODO(zmd): privatize getter
-  get url() {
-    return `${this.endpoint}?${this.params}`
+  get #fetchItemsUrl() {
+    return `${this.endpoint}?${this.#fetchItemsParams}`
   }
 
-  // TODO(zmd): privatize getter
-  get params() {
+  get #fetchItemsParams() {
     return new URLSearchParams({
       part: 'snippet',
       maxResults: this.maxResults,
