9fe06cb997c42e1b82ff77f2a148e3c4be456081
Zeb DeOs <zebdeos@bayprogrammer.com>
2025-06-02 05:07:02 -0400

    VidsiftrSearchOrder: give the component some style

--- src/components/vidsiftr-search-order.js
+++ src/components/vidsiftr-search-order.js
@@ -6,12 +6,56 @@ export default class VidsiftrSearchOrder extends LitElement {
   }
 
   static styles = css`
-    /* TODO(zmd): gimme style */
+    .active {
+      font-weight: bold;
+    }
+
+    ol {
+      list-style: none;
+      text-align: center;
+      padding: .35em;
+      padding-bottom: .5em;
+      margin: 0;
+      background: #ffc0ac;
+    }
+
+    li {
+      display: inline-block;
+    }
+
+    a {
+      text-decoration: none;
+      text-shadow: 0px 1px #f4dfd8;
+      color: #844637;
+      font-family: sans;
+      font-size: 9pt;
+      padding: 0.5em;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
   `
 
   render() {
     return html`
-      <p>Search Order Coming Soon (${this.searchOrder})</p>
+      <ol>
+        <li>
+          <a id="order-by-date"
+             class="${this.searchOrder == 'date' ? 'active' : ''}"
+             href="#">search by date</a>
+        </li>
+        <li>
+          <a id="order-by-rating"
+             class="${this.searchOrder == 'rating' ? 'active' : ''}"
+             href="#">search by rating</a>
+        </li>
+        <li>
+          <a id="order-by-relevance"
+             class="${this.searchOrder == 'relevance' ? 'active' : ''}"
+             href="#">search by relevance</a>
+        </li>
+      </ol>
     `
   }
 
